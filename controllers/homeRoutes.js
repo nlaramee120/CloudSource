39fe1782888177f9e49902e4ee07a0079e1e5c84
elliottdombrowski <elliottroyal@me.com>
2021-10-30 12:23:28 -0700

    porting html over to hbs

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,7 +4,7 @@ const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
     try {
-        res.render('main')
+        res.render('login')
     } catch (err) {
         res.status(500).json(err);
     }
