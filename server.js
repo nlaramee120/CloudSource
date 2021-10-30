2fba2371fab1ba9f6dc25ec78e38fae6bee9edbc
nlaramee120 <nlaramee120@aol.com>
2021-10-30 11:57:24 -0500

    added a path.join to the server file.

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-
 const sequelize = require('./config/connection');
 
 const app = express();
@@ -10,6 +9,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use(routes);
