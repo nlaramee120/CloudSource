313a058c4cbf9a740fd2e94cb416e97fab896ed6
elliottdombrowski <elliottroyal@me.com>
2021-10-30 11:37:16 -0700

    trying routes stuff

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
-const { ??? } = require('../models');
-// const withAuth = require('../utils/auth');
+const { Developer } = require('../models');
+const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
     try {
+        res.render('login')
     } catch (err) {
         res.status(500).json(err);
     }
