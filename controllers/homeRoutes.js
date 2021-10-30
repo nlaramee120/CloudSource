dccc356c8cc0c45da251438ac55f33a371d7e756
elliottdombrowski <elliottroyal@me.com>
2021-10-30 11:10:46 -0700

    added handlebars and homeRoutes

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const { ??? } = require('../models');
+// const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+    try {
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
