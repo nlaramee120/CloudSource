74c474060c949b73fadc1e05c06be71e293c9239
nlaramee120 <nlaramee120@aol.com>
2021-10-30 14:30:49 -0500

    resolved merge conflict

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,11 +1,6 @@
 const router = require('express').Router();
-<<<<<<< HEAD
-// const { ??? } = require('../models');
-// const withAuth = require('../utils/auth');
-=======
 const { Developer } = require('../models');
 const withAuth = require('../utils/auth');
->>>>>>> b8a4f2081bf24e2c31ed10b995241273efdc54c4
 
 router.get('/', async (req, res) => {
     try {
