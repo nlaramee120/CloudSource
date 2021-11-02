7db4ae67217339bf781e53ac442f5f6f58cc8c2b
elliottdombrowski <elliottroyal@me.com>
2021-11-02 01:00:36 -0500

    Porting Login/Signup HTML to handlebars

--- public/js/login.js
+++ public/js/login.js
@@ -1,16 +1,16 @@
-let menu = document.querySelector("#menu-btn")
-let navbar = document.querySelector(".navbar")
+let deskMenu = document.querySelector("#menu-btn")
+let deskNavbar = document.querySelector(".navbar")
 
 
-menu.onclick = () =>{
-    menu.classList.toggle("fa-times")
-    navbar.classList.toggle("active")
+deskMenu.onclick = () =>{
+    deskMenu.classList.toggle("fa-times")
+    deskNavbar.classList.toggle("active")
 }
 
 
 window.onscroll = () =>{
-    menu.classList.remove("fa-times")
-    navbar.classList.remove("active")
+    deskMenu.classList.remove("fa-times")
+    deskNavbar.classList.remove("active")
 }
 //INPUT FORM VALIDATION
 
