e6aa4d25a9fb2b8320e9c7ce62cd8d5fe4a79ea3
nlaramee120 <nlaramee120@aol.com>
2021-11-02 15:37:29 -0500

    Updated the server file.

--- server.js
+++ server.js
@@ -3,11 +3,26 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const session = require('express-session')
+const helper = require('./utils')
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create();
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+  };
+
+app.use(session(sess));
 
 //START THAT MF HANDLEBARS ENGINE
 app.engine('handlebars', hbs.engine);
