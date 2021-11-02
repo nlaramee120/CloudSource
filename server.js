a5b3dce36ab7f0d9f49a8cc04f1c78984e6ed217
nlaramee120 <nlaramee120@aol.com>
2021-11-02 18:32:46 -0500

    Commented out the helpers for now as the server does not run with them.

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const session = require('express-session')
-const helper = require('./utils')
+// const helper = require('./utils')
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
