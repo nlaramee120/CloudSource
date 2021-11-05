6dbb430da3079f3e8a2920f54d57a8540f00794e
nlaramee120 <nlaramee120@aol.com>
2021-11-04 20:34:06 -0500

    Created a route for filtering through the dev table.

--- controllers/api/devRoutes.js
+++ controllers/api/devRoutes.js
@@ -1,5 +1,8 @@
 const router = require('express').Router();
 const { Developer } = require('../../models');
+const { Sequelize } = require('sequelize');
+
+const Op = Sequelize.Op
 
 router.get('/', async (req, res) => {
     
@@ -13,6 +16,48 @@ router.get('/', async (req, res) => {
       res.status(500).json(err);
     }
   });
+
+router.get('/filter/:skills', async (req, res) => {
+  try {
+
+    let rad = req.params.skills.split("&")
+    const _test = (includedSkills) => {
+
+      let query = []
+      console.log(includedSkills)
+      includedSkills.forEach(x => {
+      query.push(
+          {skills: {
+                [Op.like]:`%${x}%`
+            }}
+        )
+      })
+      console.log(query)
+      return query
+    }
+    
+    const devData = await Developer.findAll({
+      where: {
+        [Op.and]: 
+             _test(rad)
+      }
+    }
+    );
+
+    if (!devData) {
+      res
+        .status(400)
+        .json({ message: 'There are no developers with these skills'});
+        return;
+    }
+
+    res.status(200).json(devData)
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
+})
+
 
 
 module.exports = router;
--- models/developer.js
+++ models/developer.js
@@ -1,4 +1,4 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes, Sequelize } = require('sequelize');
 const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 
