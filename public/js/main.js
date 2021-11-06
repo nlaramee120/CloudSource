53d28707362d958429461eb5bddb705fe0bcca66
elliottdombrowski <elliottroyal@me.com>
2021-11-06 12:36:22 -0500

    added basic handling to form checkboxes

--- public/js/main.js
+++ public/js/main.js
@@ -33,8 +33,44 @@ window.onclick = (event) => {
 
 //HANDLE REQ FORM 
 const handleReq = (event) => {
+    //prevent default action
     event.preventDefault();
-    console.log('testing req');
+
+    let valid = false;
+
+    if (document.getElementById('html').checked) {
+        valid = true;
+    } else if (document.getElementById('css').checked) {
+        valid = true;
+    } else if (document.getElementById('js').checked) {
+        valid = true;
+    } else if (document.getElementById('ts').checked) {
+        valid = true;
+    } else if (document.getElementById('react').checked) {
+        valid = true;
+    } else if (document.getElementById('python').checked) {
+        valid = true;
+    } else if (document.getElementById('java').checked) {
+        valid = true;
+    } else if (document.getElementById('node').checked) {
+        valid = true;
+    } else if (document.getElementById('sql').checked) {
+        valid = true;
+    } else if (document.getElementById('php').checked) {
+        valid = true;
+    } else if (document.getElementById('cpp').checked) {
+        valid = true;
+    } else {
+        console.log('check something');
+    }
+
+    if (valid) {
+        alert('validated');
+    } else {
+        alert('pick');
+        return false;
+    }
+
 }
 
 try {
