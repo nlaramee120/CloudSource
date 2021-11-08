cdee69979a1043a0f2455b64e9fc672f93033bef
elliottdombrowski <elliottroyal@me.com>
2021-11-08 16:23:07 -0600

    Refactored build req form validation to exist only in buildReq.js, added validation to reject empty inputs

--- public/js/buildreq.js
+++ public/js/buildreq.js
@@ -2,6 +2,11 @@
 const handleReq = (event) => {
     //PREVENT FORMS FROM CLEARING ON SUBMIT
     event.preventDefault();
+
+    //COLLECT VALUE OF TEXT INPUT FORMS TO VALIDATE
+    const comp = document.querySelector('#request-company-name').value.trim();
+    const title = document.querySelector('#request-title').value.trim();
+    const desc = document.querySelector('#request-description').value.trim();
     
     //CREATE ARRAY OF FORM INPUT'S IDS
     let techVal = [
@@ -23,25 +28,52 @@ const handleReq = (event) => {
     
     //LOOP LENGTH OF TECHVAL ARRAY
     for (let i = 0; i < techVal.length; i++) {
+        //GET ELEMENT BY ID BY REFERENCING TECHVAL ARRAY
         //IF ANY INDEX IS CHECKED/SELECTED, PUSH TO EMPTY CHECKEDARR
         if (document.getElementById(techVal[i]).checked) {
             checkedArr.push(techVal[i]);
         }
     };
 
+    //IF NO USER INPUT ON ANY FORM, REJECT SUBMIT AND END SCRIPT
+    if (!comp || !title || !desc || checkedArr.length <= 0) {
+        alert('Please enter all forms.');
+        return;
+    }
+
     //IF ARRAY OF CHECKED INPUTS IS > 0, HIT API AND RETURN DEVELOPERS
     if (checkedArr.length > 0) {
-        // console.log("array: " + checkedArr);
         return fetch(`api/developers/filter/${checkedArr.join("&")}`, {
             method: "GET"
         })
+        .then(function (response) {
+            return response.json();
+          })
         .then(function (data) {
-            console.log(data.json());
-        });
+            console.log(data);
 
+            var newCard = $("<div>").attr("class", "card");
+
+            $(".filteredEmps").append(newCard)
+
+            for (i = 0; i < data.length; i++) {
+                var newCardBody = $("<div>").attr("class", "card-body");
+                $(".card").append(newCardBody)
+                
+                let name = data[i].first_name + " " + data[i].last_name;
+                console.log(name)
+                newCardBody.append("<h3 class='card-title'>" + name + "<h3>")
+
+                let email = data[i].email;
+                newCardBody.append("<p class='card-text'>" + email + "<p>");
+
+                let skills = data[i].skills;
+                newCardBody.append("<p class='card-text'>" + skills + "<p>");
+            }
+        })
     } else {
         //OTHERWISE FORCE USER TO SELECT SOMETHING
-        alert('Must Select At Least One Tech.');
+        alert('Must select at least one tech.');
         return false;
     }
 };
